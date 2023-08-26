f13994e6c498fa6bd29a4515720127c8332eca9c
Edward Guilfoyle <edwardguil96@outlook.com>
2023-08-26 11:28:37 +1000

    Added pipeline (realsense -> mediapipe) for extracting left/right hand centeral position (x,y + depth)

---
+++ python/realsense.py
@@ -0,0 +1,75 @@
+import pyrealsense2 as rs
+import mediapipe as mp
+import cv2
+import numpy as np
+
+# ====== Realsense ======
+realsense_ctx = rs.context()
+connected_devices = [realsense_ctx.devices[i].get_info(rs.camera_info.serial_number) for i in range(len(realsense_ctx.devices))]
+device = connected_devices[0]  # In this example we are only using one camera
+pipeline = rs.pipeline()
+config = rs.config()
+
+# ====== Mediapipe ======
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+mpFace = mp.solutions.face_mesh
+face = mpFace.FaceMesh()
+
+
+# ====== Enable Streams ======
+config.enable_device(device)
+stream_res_x = 640
+stream_res_y = 480
+stream_fps = 30
+
+config.enable_stream(rs.stream.depth, stream_res_x, stream_res_y, rs.format.z16, stream_fps)
+config.enable_stream(rs.stream.color, stream_res_x, stream_res_y, rs.format.bgr8, stream_fps)
+profile = pipeline.start(config)
+
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+# ====== Get depth Scale ======
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+
+print(f"Starting to capture images on SN: {device}")
+
+while True:
+    # Get and align frames
+    frames = pipeline.wait_for_frames()
+    aligned_frames = align.process(frames)
+    aligned_depth_frame = aligned_frames.get_depth_frame()
+    color_frame = aligned_frames.get_color_frame()
+    
+    if not aligned_depth_frame or not color_frame:
+        continue
+
+    # Process images
+    depth_image = np.asanyarray(aligned_depth_frame.get_data())
+    depth_image_flipped = cv2.flip(depth_image, 1)
+    color_image = np.asanyarray(color_frame.get_data())
+    color_images_rgb = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+
+    # Process hands
+    results = hands.process(color_images_rgb)
+    if results.multi_hand_landmarks:
+        for idx, handLms in enumerate(results.multi_hand_landmarks):
+            hand_side_classification_list = results.multi_handedness[idx]
+            hand_side = hand_side_classification_list.classification[0].label
+            middle_finger_knuckle = handLms.landmark[9]
+            x = int(middle_finger_knuckle.x * len(depth_image_flipped[0]))
+            y = int(middle_finger_knuckle.y * len(depth_image_flipped))
+            mfk_distance = depth_image_flipped[y,x] * depth_scale  # meters
+            print(f"{hand_side} Hand Midpoint (x: {x}, y: {y}), Distance: {mfk_distance:.3f} meters")
+
+    key = cv2.waitKey(1)
+    # Press esc or 'q' to exit the loop
+    if key & 0xFF == ord('q') or key == 27:
+        break
+
+print(f"Application Closing")
+pipeline.stop()
+print(f"Application Closed.")
