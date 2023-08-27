8d6bf856307fa559a71a27f9afe2fb2fee73df55
Ahmad Abu-Aysha <s4638852@student.uq.edu.au>
2023-08-27 11:40:14 +1000

    more audio assets

--- kybo/src/Main.gd
+++ kybo/src/Main.gd
@@ -20,7 +20,7 @@ func _on_boxer1_die():
 	$UserInterface/Retry.show()
 
 
-func _unhandled_input(event):
+func _input(event):
 	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
 		# Restart the scene
 		get_tree().reload_current_scene()
