c6d47820abe25092b4d9ec9d89a3dda3cacf218c
Kaz-Scherger <n11013630@qut.edu.au>
2023-08-27 11:57:24 +1000

    Added bg texture for shits and giggles

--- kybo/src/Global.gd
+++ kybo/src/Global.gd
@@ -4,7 +4,7 @@ var host  = null
 var opponent = null
 
 var network : NetworkedMultiplayerENet
-var ip : String = "127.0.0.1"
+var ip : String = "10.89.140.109" # "127.0.0.1"
 var port : int = 1909
 var max_players : int = 2
 
