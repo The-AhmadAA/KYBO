63655f9cb71886abd840d63749c09a81deabb346
Kaz-Scherger <n11013630@qut.edu.au>
2023-08-27 14:38:37 +1000

    Musky Zuccy 4

--- kybo/src/Main Scenes/Level.gd
+++ kybo/src/Main Scenes/Level.gd
@@ -8,7 +8,9 @@ func set_character(character : String) -> void:
 			$Musk.set_script(load("res://Boxer/RealsenseBoxer.gd"))
 			$Zucc.set_script(load("res://Boxer/OpponentBoxer.gd"))
 			$Zucc.add_to_group("opponent")
+			$Musk/Face/Camera.current = true
 		"Zucc":
 			$Zucc.set_script(load("res://Boxer/RealsenseBoxer.gd"))
 			$Musk.set_script(load("res://Boxer/OpponentBoxer.gd"))
 			$Musk.add_to_group("opponent")
+			$Zucc/Face/Camera.current = true
