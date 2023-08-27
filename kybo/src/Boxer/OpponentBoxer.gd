cfe5a1c7114dabf656501be049e2c07c506377e4
Edward Guilfoyle <edwardguil96@outlook.com>
2023-08-27 11:55:52 +1000

    Disable updating positions until player state is recieved

--- kybo/src/Boxer/OpponentBoxer.gd
+++ kybo/src/Boxer/OpponentBoxer.gd
@@ -34,7 +34,8 @@ onready var hand_left : KinematicBody = $Hand_Left
 
 func _process(delta):
 	# Check that there is an opponent to move
-	update_hands_position()
+	if Global.opponent != null:
+		update_hands_position()
 
 func recieve_state(player_state) -> void:
 	var time = player_state['time']
@@ -77,7 +78,7 @@ func update_hands_position():
 	#		averaged_position += pos
 	#	averaged_position /= position_history[type].size()
 	
-	
-	head.translation = position_history['Face'][-1]
-	hand_left.translation = position_history['Hand_Left'][-1] + Vector3(1.5, 0.0, -4.0)
-	hand_right.translation = position_history['Hand_Right'][-1] + Vector3(1.0, 0.0, -4.0)
+	if prev_time_recieved != 0:
+		head.translation = position_history['Face'][-1]
+		hand_left.translation = position_history['Hand_Left'][-1] + Vector3(1.5, 0.0, -4.0)
+		hand_right.translation = position_history['Hand_Right'][-1] + Vector3(1.0, 0.0, -4.0)
