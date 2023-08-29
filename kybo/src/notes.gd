13fdf09e0e9b1e28ccabdca2eda379f8ad1dd76f
Kaz-Scherger <n11013630@qut.edu.au>
2023-08-30 00:53:05 +1000

    Added notes for wanted features / todo

---
+++ kybo/src/notes.gd
@@ -0,0 +1,32 @@
+"""
+TODO:
+	Integrate damage:
+		[ ] bring in health
+		[ ] couple health with UI bars
+		[ ] bring in red flashing
+		[ ] bring in score
+		[ ] bring in "death" (either animation or hiding of loser player)
+		
+		Extension of integration:
+			[ ] figure out how to reset scene for rematch
+	
+	Dynamic levels:
+		mostly so that single player can have unique function
+		[ ] base level seperation?
+			[ ] add another level to prove that you can c:
+		[ ] spawn necessary models into scene
+			[ ] with necessary scripts and settings
+	
+	Character customisation
+		[ ] edit origin of Elon and Mark heads (to be dynamically added to boxer)
+		[ ] allow (host) player to choose head?
+		[ ] add more heads?
+	
+	Misc:
+		[ ] customise IP and PORT in UI
+		[ ] add settings?
+			[ ] volume settings
+			[ ] 
+		[ ] add asset links to readme to credit properly (currently, academic misconducting!)
+		[ ] improve webcam experience
+"""
