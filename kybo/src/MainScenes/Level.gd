e9a8e2fbaf80b687ae98725579c93cd90efc44ee
Kaz-Scherger <n11013630@qut.edu.au>
2023-08-30 00:24:54 +1000

    Quick correction. adding player to player group

--- kybo/src/MainScenes/Level.gd
+++ kybo/src/MainScenes/Level.gd
@@ -8,6 +8,7 @@ func set_character(character : String) -> void:
 		"Single", "Musk":
 			$Musk.set_script(load("res://Boxer/RealsenseBoxer.gd"))
 			$Zucc.set_script(load("res://Boxer/OpponentBoxer.gd"))
+			$Musk.add_to_group("player")
 			$Zucc.add_to_group("opponent")
 			$Musk/Face/Camera.current = true
 		"Zucc":
